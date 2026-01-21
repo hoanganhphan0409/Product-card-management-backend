4ba8b15e545233251f96ad137d847678f2acc8bc
anhphgameestudio <89737507+hoanganhphan0409@users.noreply.github.com>
2026-01-21 21:12:43 +0700

    first commit

---
+++ src/main/java/com/example/backend_product_card_management/BackendProductCardManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_product_card_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProductCardManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProductCardManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backend_product_card_management/BackendProductCardManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_product_card_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProductCardManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
